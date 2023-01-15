5ea62cc862350c0878c6b70c2b8858c87aea4384
MauroDoneda <mauro.doneda@gmail.com>
2023-01-15 20:07:59 +0100

    Add products context and products card component

--- clothes-store/src/contexts/user.context.jsx
+++ clothes-store/src/contexts/user.context.jsx
@@ -1,5 +1,5 @@
 import {createContext, useEffect, useState} from "react";
-import {createUserDoc, onAuthStateChangeListener, signOutUser} from "../utils/firebase/firebase.utils";
+import {createUserDoc, onAuthStateChangeListener} from "../utils/firebase/firebase.utils";
 
 export const UserContext = createContext({
     currentUser: null,
--- clothes-store/src/routes/shop/shop.component.jsx
+++ clothes-store/src/routes/shop/shop.component.jsx
@@ -1,10 +1,18 @@
-
+import {ProductsContext} from "../../contexts/products.context";
+import {useContext} from "react";
+import ProductCard from "../../components/product-card/product-card.component";
+import './shop.styles.scss'
 
 const Shop = () => {
+    const {products} = useContext(ProductsContext);
 
     return (
-        <div>
-            <h1>Shop</h1>
+        <div className={'products-container'}>
+            {products.map(( product ) =>
+                <div key={product.id}>
+                    <ProductCard product={product}/>
+                </div>
+            )}
         </div>
     )
 }
